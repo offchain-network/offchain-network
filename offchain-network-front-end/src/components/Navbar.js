c40431440fbb6e5569fc44d375e23b77c2b7b978
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-18 00:27:42 +0200

    add nav-links and nav-center

--- offchain-network-front-end/src/components/Navbar.js
+++ offchain-network-front-end/src/components/Navbar.js
@@ -4,11 +4,23 @@ import '../css/navbar.css';
 
 const Navbar = () => {
   return ( 
-    <div>
+    <div className="navbar-container">
       <nav>
-        <div className = "nav-logo">
-          <img src={Logo} alt="logo"/>
-          <span>offchain.network</span>
+        <div className="nav-description">
+          <div className="nav-logo">
+            <img src={Logo} alt="logo"/>
+            <span>offchain.network</span>
+          </div>
+          <div className="nav-center">
+            <span><a href="#">Create Channel</a></span>
+            <span><a href="#">Send</a></span>
+            <span><a href="#">Receive</a></span>
+          </div>
+        </div>
+        <div className="nav-links">
+          <span><a href="#">About</a></span>
+          <span><a href="#">Whitepaper</a></span>
+          <span><a href="#">Contact</a></span>
         </div>
       </nav> 
     </div>
