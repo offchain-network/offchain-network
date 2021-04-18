c8a305e693eb431ccb3899d63de3d6a8ccffd5e9
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-18 02:25:14 +0200

    add flex design to the app.js

--- offchain-network-front-end/src/App.js
+++ offchain-network-front-end/src/App.js
@@ -1,11 +1,16 @@
+import "./css/app.css";
 import Navbar from "./components/Navbar";
+import Receiver from "./components/Receiver";
 import Sender from "./components/Sender";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Sender/>
+      <div className="transfer-info">
+        <Sender/>
+        <Receiver/>
+      </div>
     </div>
   );
 }
