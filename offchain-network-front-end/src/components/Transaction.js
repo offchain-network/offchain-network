933a0a393891819ed5149f21b1ad58a29f471d4b
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-18 23:25:19 +0200

    add modification to the previous design

---
+++ offchain-network-front-end/src/components/Transaction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Transaction = () => {
+  return ( 
+    <div className="history-container">
+      <div className="history-title">
+        <span>Transaction History</span>
+        <span><i className="fas fa-sync-alt"></i></span>
+      </div>
+    </div>
+   );
+}
+ 
+export default Transaction;
