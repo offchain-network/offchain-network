fcac7558601bcb05157e84be02f526b864105d77
kristofgazso <kristof.gazso@gmail.com>
2021-04-23 09:33:22 +0100

    Added optimism, zinc and matic support, began writing zinc code

--- contracts/solidity/scripts/deploy.js
+++ contracts/solidity/scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Hub = await hre.ethers.getContractFactory("UnidirectionalPaymentChannelHub");
+  const hub = await Hub.deploy();
 
-  await greeter.deployed();
+  await hub.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("UnidirectionalPaymentChannelHub deployed to:", hub.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
