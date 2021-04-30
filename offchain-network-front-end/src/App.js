d6c084cfeb16a6d085c3a36d9f2ef0bc2330e5ef
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-30 16:22:04 +0200

    add send payment button functionality

--- offchain-network-front-end/src/App.js
+++ offchain-network-front-end/src/App.js
@@ -4,13 +4,29 @@ import Receiver from "./components/Receiver";
 import Sender from "./components/Sender";
 import Transaction from "./components/Transaction";
 import arrow from "./images/Forward Arrow.png";
+import { useState, useEffect } from "react";
+import { ethers } from 'ethers';
 
 function App() {
+
+const [signer, setSigner] = useState(undefined);
+
+const init = async () => {
+  await window.ethereum.enable()
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  setSigner(signer);
+}
+
+useEffect(()=>{
+  init();
+}, [])
+
   return (
     <div className="App">
       <Navbar/>
       <div className="transfer-info">
-        <Sender/>
+        <Sender signer={signer}/>
         <img src={arrow} alt="arrow" className="arrow-png"/>
         <Receiver/>
       </div>
--- offchain-network-front-end/src/components/Sender.js
+++ offchain-network-front-end/src/components/Sender.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
 import "../css/sender.css";
 
-const Sender = () => {
+const Sender = ({signer}) => {
+
+  const [amount, setAmount] = useState(0.0);
+
+  const constructPaymentMessage = async (contractAddress, channelId, amount) => {
+    return ethers.utils.solidityKeccak256(
+        ["address", "bytes32", "uint256"],
+        [contractAddress, channelId, amount]
+    );
+}
+
+const signPayment = async (signer, contractAddress, channelId, amount) => {
+    let message = await constructPaymentMessage(contractAddress, channelId, amount);
+    let signedMessage = await signer.signMessage(ethers.utils.arrayify(message));
+    return signedMessage;
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  signPayment(signer, "0x0000000000000", "channelId", amount);
+}
+
+const handleChange = (e) => {
+  const amount = e.target.value;
+  setAmount(amount);
+}
+
   return ( 
     <div className="sender-container">
       <span>You</span>
       <div className="send-description">
-        <form className="send-info">
+        <form className="send-info" onSubmit={handleSubmit}>
           <div className="form-border">
             <div className="form-part">
               <span>Amount</span>
-              <input placeholder="0.00"/>
+              <input value={amount} onChange={handleChange} name="amount"/>
             </div>
             <div className="form-part">
               <span>Balance: 1.1183</span>
