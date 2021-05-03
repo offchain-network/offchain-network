bd52c362157f316daef218973cc0e1ddf835d390
NtwaliHeritier <heritierntwali@gmail.com>
2021-05-03 19:04:23 +0200

    add toggle on part

--- offchain-network-front-end/src/components/Receive.js
+++ offchain-network-front-end/src/components/Receive.js
@@ -24,6 +24,16 @@ const Receive = () => {
           <label>Balance</label>
           <input placeholder="1.1103 ETH"/>
         </div>
+        <div className="receive-form-on">
+          <div className="amount-to-withdraw">
+            <span>Amount to withdraw</span>
+            <input placeholder="31.862332423846223847 ETH"/>
+          </div>
+          <div className="amount-to-withdraw">
+            <span>Withdrawal Signature</span>
+            <input className="input" placeholder="0x8a3cd88e97634c89b3a5d8408dcb73013e7357ae92f5d6"/>
+          </div>
+        </div>
         <button>Redeem</button>
       </form>
     </div>
