518c5dfb4594049aadb8c2291b781e716585dae0
Benjamin Ebner <ebner.benjamin@gmail.com>
2021-05-06 09:54:05 +0200

    Adding channelRoutes

---
+++ aws-serverless/src/frameworks/web/routes/channelRoutes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import channelController from "../../../controllers/channelController";
+import { IDependencies } from "../../../types/types";
+import authMiddlewares from "../middlewares/authMiddlewares";
+
+const channelRoutes = (dependencies: IDependencies) => {
+  const router = express.Router();
+
+  const { isAuthorized } = authMiddlewares(dependencies);
+
+  const controller = channelController(dependencies);
+
+  router
+    .route("/")
+    .all(isAuthorized)
+    .get(controller.listChannels)
+    .post(controller.createChannel)
+    .delete(controller.deleteChannel);
+
+  return router;
+};
+
+export default channelRoutes;
