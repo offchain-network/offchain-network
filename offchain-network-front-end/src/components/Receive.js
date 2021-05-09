695ad9cf674b5965d7008041eef87447f9fe9eca
NtwaliHeritier <heritierntwali@gmail.com>
2021-05-09 18:45:16 +0200

    add changes to the style

--- offchain-network-front-end/src/components/Receive.js
+++ offchain-network-front-end/src/components/Receive.js
@@ -142,20 +142,9 @@ const Receive = () => {
     <div className="receive-container">
       <div className="receive-header">
         <span>You</span>
-        <div className="custom">
-          <span>Custom</span>
-           <div class="custom-control custom-switch">
-              <input type="checkbox" class="custom-control-input" id="customSwitch1" onChange={handleChange}/>
-              <label class="custom-control-label" htmlFor="customSwitch1"></label>
-          </div>
-        </div>
       </div>
       <form>
         <div className={`receive-form-off ${display ? "display" : ""}`}>
-          <label>Balance</label>
-          <input placeholder="0.00 ETH"/>
-        </div>
-        <div className={`receive-form-on ${display ? "" : "display"}`}>
           <div className="amount-to-withdraw">
             <span>Amount to withdraw</span>
             <div className="matic-div"><input className="input" placeholder="31.862332423846223847" name="amount" onChange={handleInput} value={ChannelId.amount}/><span>MATIC</span></div>
